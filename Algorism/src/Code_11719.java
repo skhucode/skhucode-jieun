55f087b913bb458812371f3a17d060b229fe668a
JiEun <ys6445@naver.com>
2018-07-27 18:45:20 +0900

    알고리즘 기초

--- Algorism/src/Code_11719.java
+++ Algorism/src/Code_11719.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-// 그대로 출력하기
+// 그대로 출력하기2
 public class Code_11719 {
 
 	public static void main(String[] args) {
@@ -8,11 +8,9 @@ public class Code_11719 {
 
 		Scanner s = new Scanner(System.in);
 		
-		String a;
-		
 		while(s.hasNext()) {
-			a = s.nextLine();
-			System.out.println(a);
+
+			System.out.println(s.nextLine());
 		}
 	}
 
