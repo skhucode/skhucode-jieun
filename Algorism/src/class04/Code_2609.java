a751a557f5782360df1fb9b77a434d9d47b8f5e9
JiEun <ys6445@naver.com>
2018-09-02 09:50:22 +0900

    트리
    트리 알고리즘 학습

--- Algorism/src/class04/Code_2609.java
+++ Algorism/src/class04/Code_2609.java
@@ -1,4 +1,4 @@
-package class03;
+package class04;
 
 import java.util.Scanner;
 
--- Algorism/src/class07/Code_1967.java
+++ Algorism/src/class07/Code_1967.java
@@ -38,11 +38,68 @@ package class07;
 출력
 45*/
 
+import java.util.*;
+class EdgeTwo{
+	public int to;
+	public int cost;
+	EdgeTwo(int to, int cost){
+		this.to = to;
+		this.cost = cost;
+	}
+}
 public class Code_1967 {
-
+	public static int[] bfs(int n, List<EdgeTwo>[] a, int start) {
+		boolean[] check  = new boolean[n+1];
+		int[] dist = new int[n+1];
+		Queue<Integer> q = new LinkedList<Integer>();
+		check[start] =true;
+		q.add(start);
+		while(!q.isEmpty()) {
+			int x = q.remove();
+			for(EdgeTwo e: a[x]) {
+				int y = e.to;
+				int cost = e.cost;
+				if(check[y] == false) {
+					dist[y] = dist[x] + cost;
+					q.add(y);
+					check[y] =true;
+				}
+			}
+		}
+		return dist;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		List<EdgeTwo>[] a = (List<EdgeTwo>[]) new List[n+1];
+		for(int i=1; i<=n; i++) {
+			a[i] = new ArrayList<EdgeTwo>();
+		}
+		for(int i=1; i<= n; i++) {
+			int x = sc.nextInt();
+			while(true) {
+				int y = sc.nextInt();
+				if(y == -1) break;
+				int z = sc.nextInt();
+				a[x].add(new EdgeTwo(y,z));
+			}
+		}
+		int[] dist = bfs(n,a,1);
+		int start = 1;
+		for(int i=2; i<=n; i++) {
+			if(dist[i] > dist[start]) {
+				start = i;
+			}
+		}
+		dist = bfs(n,a,start);
+		int ans = dist[1];
+		for(int i=2; i<=n; i++) {
+			if(ans < dist[i]) {
+				ans = dist[i];
+			}
+		}
+		System.out.println(ans);
 	}
 
 }
